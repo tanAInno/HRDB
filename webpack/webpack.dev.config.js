67a91a2d56d1b2b079e6d0b4dd7791b1f96a021d
tanAInno <natanon.p@ku.th>
2018-09-18 09:13:06 +0700

    change host port setting

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -3,6 +3,16 @@ var path = require('path');
 
 var parentDir = path.join(__dirname, '../');
 
+const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+
+const METADATA = Object.assign({}, {
+    host: HOST,
+    port: PORT,
+    PUBLIC: process.env.PUBLIC_DEV || HOST + ':' + PORT
+});
+
 module.exports = {
     entry: [
         'babel-polyfill',
@@ -32,6 +42,9 @@ module.exports = {
     devServer: {
         inline: false,
         contentBase: parentDir,
-        historyApiFallback: true
+        historyApiFallback: true,
+        port: METADATA.port,
+        host: METADATA.host,
+        public: METADATA.PUBLIC,
     }
 }
